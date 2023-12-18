692f1a388c4e157c23411f9fba80acbac0df0c1a
TimoSP9 <tseptian@student.ciputra.ac.id>
2023-12-18 10:59:23 +0700

    home page

--- DishDiscovery/DishDiscoveryApp.swift
+++ DishDiscovery/DishDiscoveryApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DishDiscoveryApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomePage()
                 .environment(\.colorScheme, .dark)
         }
     }
--- DishDiscovery/Main/HomePage.swift
+++ DishDiscovery/Main/HomePage.swift
@@ -8,9 +8,107 @@
 import SwiftUI
 
 struct HomePage: View {
+    @State private var searchFood: String = ""
         
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       
+        Group {
+            VStack {
+                
+                HStack {
+                    
+                    Text("Hello, ")
+                    Spacer()
+                    Image(systemName: "person.fill")
+                        .resizable()
+                        .frame(width: 56, height: 56, alignment: .center)
+                        
+                }
+                .padding()
+                
+                TextField("Search...", text: $searchFood)
+                    
+                    .padding()
+            }
+            
+        }
+        Spacer()
+        Group{
+            VStack{
+                
+                HStack{
+                    Text("Recommendation")
+                        .font(.title2)
+                    Spacer()
+                    Button(action: {
+                        
+                    }, label: {
+                            Text("See all")
+                        
+                    })
+                    .padding()
+                }
+                    
+                ScrollView([.horizontal]){
+                    HStack  {
+                        ForEach(0..<100) {
+                            Text("Row \($0)")
+                                .foregroundColor(.white)
+                        }
+                    }
+                    .padding()
+                }
+            }
+        }
+        Spacer()
+        
+        Group{
+            
+            VStack{
+                Text("Popular Category")
+                    .padding()
+                    .font(.title2)
+                    .frame(maxWidth:.infinity, alignment: .leading)
+                ScrollView([.horizontal]){
+                    HStack{
+                        Button(action: {
+                            
+                        }, label: {
+                                Text("Breakfast")
+                            
+                        })
+                        .padding()
+                        Button(action: {
+                            
+                        }, label: {
+                                Text("Lunch")
+                            
+                        })
+                        .padding()
+                        Button(action: {
+                            
+                        }, label: {
+                                Text("Dinner")
+                            
+                        })
+                        .padding()
+                        Button(action: {
+                            
+                        }, label: {
+                                Text("Appetizer")
+                            
+                        })
+                        .padding()
+                    }
+                }
+                Grid{
+                    GridRow{
+                        Text("")
+                    }
+                }
+            }
+            Spacer()
+        }
     }
 }
 
